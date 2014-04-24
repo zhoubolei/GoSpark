1df915c34d6243ef8e1445d89855ee2c611a6a4d
Bolei <hduser@>
2014-04-24 16:12:35 -0400

    small fix in test

--- Spark/src/hadoop/test_test.go
+++ Spark/src/hadoop/test_test.go
@@ -9,16 +9,14 @@ func TestBasicRead(t *testing.T) {
 //    fileURI := "hdfs://127.0.0.1:54310/user/hduser/testSplitRead.txt";
   fileURI := "hdfs://vision24.csail.mit.edu:54310/user/featureSUN397.csv";
 
-  fmt.Printf("1\n");
 	s := getSplitInfo(fileURI)
-  fmt.Printf("2\n");
 	nsplit := s.Len();
 	
 	fmt.Printf("This file has %d splits\n", nsplit);
 	for it := s.Front(); it != nil; it=it.Next() {
 	    slist := it.Value.([]string)
 	    for j:=0; j<len(slist); j++ {
-	        fmt.Printf(slist[j]);
+	        fmt.Printf("%v ", slist[j]);
 	    }
 	    fmt.Println();
 	}

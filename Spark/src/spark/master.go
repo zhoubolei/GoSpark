c112eb7e405b26a74ee88972036feca7c9f7c2ad
Jiasi Shen <jiasi@mit.edu>
2014-05-09 11:24:08 -0400

    master assigns a job to a list of potential workers, select worker according to current CPU use

--- Spark/src/spark/master.go
+++ Spark/src/spark/master.go
@@ -12,7 +12,7 @@ import (
 
 type WorkerInfo struct {
   address string // addr:port of the worker, e.g. "127.0.0.1:1234"
-  nCore int      // TODO implement worker threads
+  nCore int      // for load-balancing on workers
   running int // number of jobs currently running
   memUse uint64 // amount of memory currently using
 }
@@ -125,8 +125,7 @@ func (mr *Master) WorkersAvailable() map[string]WorkerInfo {
   return m
 }
 
-// peterkty: need to use nCore to assign nCore jobs to this worker
-func (mr *Master) AssignJob(w string, args *DoJobArgs, reply *DoJobReply) bool {
+func (mr *Master) assign_to_worker(w string, args *DoJobArgs, reply *DoJobReply) bool {
   ok := call(w, "Worker.DoJob", args, reply)
   trial := 0
   for !ok && trial < 10 {
@@ -144,6 +143,89 @@ func (mr *Master) AssignJob(w string, args *DoJobArgs, reply *DoJobReply) bool {
   } else { // current job is done, ready for the next job
     DPrintf("worker %s args %v reply %v", w, args, reply)
     return true
+  }
+}
+
+func (mr *Master) assign_if_capable(w string, args *DoJobArgs, reply *DoJobReply) bool {
+  mr.mu.RLock()
+  info, avail := mr.workers[w]
+  mr.mu.RUnlock()
+  if !avail { // not available at all
+    return false
+  }
+  if info.running >= info.nCore { // all cores are busy
+    return false
+  }
+  // have idle cores, good to assign
+  return mr.assign_to_worker(w, args, reply)
+}
+
+func (mr *Master) find_least_load(workersToConsider []string) string {
+  min_load := float32(-1)
+  min_worker := ""
+  for _, w := range workersToConsider {
+    mr.mu.RLock()
+    info, avail := mr.workers[w]
+    mr.mu.RUnlock()
+    if !avail {
+      continue
+    }
+    load := float32(info.running) / float32(info.nCore)
+    if min_load < 0 || load < min_load {
+      min_load = load
+      min_worker = w
+    }
+  }
+  return min_worker
+}
+
+func (mr *Master) AssignJob(workersPreferred []string, force bool, args *DoJobArgs, reply *DoJobReply) (bool, string) {
+  // use nCore to assign nCore jobs to select workers, return the worker chosen
+
+  // first try all preferred workers
+  if force { // can only use the preferred workers, try them round-robin
+    for _, w := range workersPreferred {
+     if mr.assign_if_capable(w, args, reply) {
+        return true, w
+      }
+    }
+    // none succeeded
+    return false, ""
+  } else { // not forced, i.e. if the preferred workers not capable, allowed to use others
+    // first try the preferred workers if any of them is capable
+    for _, w := range workersPreferred {
+      if mr.assign_if_capable(w, args, reply) {
+        return true, w
+      }
+    }
+    // then try all workers if any of them is capable
+    all := mr.WorkersAvailable()
+    for w := range all {
+      if mr.assign_if_capable(w, args, reply) {
+        return true, w
+      }
+    }
+    // if still fail, pick the preferred worker with lightest load
+    w := mr.find_least_load(workersPreferred)
+    // if no preferred workers are available, pick any worker with the lightest load
+    if w == "" {
+      all = mr.WorkersAvailable() // refresh
+      all_avail := make([]string, 0)
+      for w_avail := range all {
+        all_avail = append(all_avail, w_avail)
+      }
+      w = mr.find_least_load(all_avail)
+      // if no any worker available by this time
+      if w == "" {
+        return false, ""
+      }
+    }
+    // assign to this worker with lightest load
+    if mr.assign_to_worker(w, args, reply) {
+      return true, w
+    } else {
+      return false, ""
+    }
   }
 }
 

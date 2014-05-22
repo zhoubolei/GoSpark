9e5a0bab6d35da6e9ad70f7cf85e634e87805118
Bolei Zhou <zhoubolei@gmail.com>
2014-05-22 00:05:43 -0400

    Update shell_test.sh
    remove uname

--- shells/shell_test.sh
+++ shells/shell_test.sh
@@ -4,7 +4,7 @@
 
 
 hosts=(vision25 vision26 vision27 vision29 vision30 vision31 vision32 vision33 vision34 vision36 vision37 vision38)
-UNAME=hduser
+UNAME=
 for i in "${hosts[@]}"
 do
 scp -r /scratch/bolei/hadoop $UNAME@$i:/scratch/bolei/
